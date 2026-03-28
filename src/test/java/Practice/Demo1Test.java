717aa291f77e7add360683cd599344697f6e401c
praju <praju@DESKTOP-OEBGE19>
2026-03-28 10:25:39 +0530

    first time committing

---
+++ src/test/java/Practice/Demo1Test.java
@@ -0,0 +1,13 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class Demo1Test {
+
+	@Test(groups = "smoke")
+	
+	public void Dhurandhar()
+	{
+		System.out.println("Ranveer");
+	}
+}
---
+++ src/test/java/Practice/DemoTest.java
@@ -0,0 +1,15 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+	@Test(groups="regression")
+	
+	
+	
+	public void KGFTest()
+	{
+		System.out.println("Yash");
+	}
+}
